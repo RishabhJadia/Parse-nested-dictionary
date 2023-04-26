ca1193e51ab65cc5fc718328c4da39cdb5fc4374
rja-odoo <26570284+RishabhJadia@users.noreply.github.com>
2023-04-26 12:07:16 +0530

    Update dictionay_concept.py

--- dictionay_concept.py
+++ dictionay_concept.py
@@ -21,3 +21,23 @@ for app_key, ports_ips in output.items():
         result[app_key][-1]['ips'].append(ip)
 
 print(result)
+
+----------------------------------------------------------------------------------------
+
+D1 = {'1414': [1, 2], '1413': [3, 4]}
+D2 = {1: 'app_id', '2': 'appid', 3: 'app_id', 4: 'app_id'}
+
+output = {}
+for key, values in D1.items():
+    for value in values:
+        app_key = D2.get(value)
+        if app_key:
+            if app_key not in output:
+                output[app_key] = []
+            existing_port = next((d for d in output[app_key] if d['port'] == int(key)), None)
+            if existing_port:
+                existing_port['ips'].append(value)
+            else:
+                output[app_key].append({'port': int(key), 'ips': [value]})
+
+print(output)
