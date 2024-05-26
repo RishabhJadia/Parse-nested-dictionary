18ee6c55e6f2107648c3768522480217777a41d0
rja-odoo <26570284+RishabhJadia@users.noreply.github.com>
2024-05-26 20:32:23 +0530

    Update main.go

--- Golang/booking-app/main.go
+++ Golang/booking-app/main.go
@@ -1,113 +1,163 @@
+// https://www.youtube.com/watch?v=yyUHQIec83I
 package main
 
-import (
-	"bytes"
-	"crypto/rsa"
-	"crypto/sha1"
-	"crypto/x509"
-	"encoding/json"
-	"encoding/pem"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
-)
-
-var (
-	private *rsa.PrivateKey
-	public  *rsa.PublicKey
-)
+import "fmt"
 
 func main() {
-	var err error
-	var priv []byte
-	priv, err = os.ReadFile("pki")
-	if err != nil {
-		panic(err)
-	}
-	private, err = jwt.ParseRSAPrivateKeyFromPEM(priv)
-	if err != nil {
-		panic(err)
-	}
+	// type 1 declaration of type
+	// var conferenceName = "Go Conference" // I want to use this words in many places
+	// const conferenceTickets = 50         // constants are like variables, but their values cannot be changed
+	// var remainingTickets = 50
 
-	data := map[string]interface{}{
-		"jti": "id001",
-		"aud": "/token",
-		"sub": "88153",
-		"iss": "88153",
-	}
-	cl := jwt.MapClaims{}
-	for k, v := range data {
-		cl[k] = v
-	}
-	cl["iat"] = time.Now().Add(-6 * time.Hour).Unix()
-	cl["exp"] = time.Now().UTC().Add(1 * time.Hour).Unix()
-	token := jwt.NewWithClaims(jwt.GetSigningMethod("RSA256"), cl)
-	kid := getSha1()
-	token.Header["kid"] = kid
+	// type 2 declaration of type
+	// var conferenceName string = "Go Conference" // I want to use this words in many places
+	// const conferenceTickets int = 50            // constants are like variables, but their values cannot be changed
+	// var remainingTickets int = 50
 
-	tokenString, err := token.SignedString(private)
-	fmt.Printf("Token String--->%v\n", tokenString)
-	GetTokenbyte(tokenString)
+	// type 3 declaration of type shorthand
+	conferenceName := "Go Conference" // I want to use this words in many places
+	const conferenceTickets int = 50  // constants are like variables, but their values cannot be changed
+	var remainingTickets uint = 50
+	// type 1 of printing
+	// fmt.Println("Welcome to our", conferenceName, "booking application")
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	// fmt.Println("Get your tickets here to attend")
+
+	// type 2 of printing
+	// formatted output
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	for {
+		// Array it required fixed size, only same datatype can be stored
+		// disadvantage of array: if we dont know the size of array
+		// type 1 of declaration array
+		// var bookings = [50]string{}
+		// var bookings [50]string  //array
+		// type 2 of declaration array
+		// slice is an abstraction of array. slice are more flexible and powerful. dynamic in nature i.e., re-sized when needed
+		// type 1 declaration of slice
+		// var bookings []string //slice
+		// type 2 declaration of slice
+		// var bookings = []string{} //slice
+		// type 3 declaration of slice
+		bookings := []string{} //slice
+
+		// Data types
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		// getting user input
+		fmt.Println("Enter your firstName: ")
+		fmt.Scan(&firstName) //store variable in memory address
+		fmt.Println("Enter your lastName: ")
+		fmt.Scan(&lastName) //store variable in memory address
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email) //store variable in memory address
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets) //store variable in memory address
+
+		remainingTickets = remainingTickets - userTickets
+		// bookings[0] = firstName + " " + lastName  //array
+		bookings = append(bookings, firstName+" "+lastName) //slice
+		// fmt.Printf("The whole array: %v\n", bookings)
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// fmt.Printf("Array Type: %T\n", bookings)
+		// fmt.Printf("Length of Array: %v\n", len(bookings))
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice Type: %T\n", bookings)
+		fmt.Printf("Length of Slice: %v\n", len(bookings))
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
+	// // Array it required fixed size, only same datatype can be stored
+	// // disadvantage of array: if we dont know the size of array
+	// // type 1 of declaration array
+	// // var bookings = [50]string{}
+	// // var bookings [50]string  //array
+	// // type 2 of declaration array
+	// // slice is an abstraction of array. slice are more flexible and powerful. dynamic in nature i.e., re-sized when needed
+	// // type 1 declaration of slice
+	// // var bookings []string //slice
+	// // type 2 declaration of slice
+	// // var bookings = []string{} //slice
+	// // type 3 declaration of slice
+	// bookings := []string{} //slice
+
+	// // Data types
+	// var firstName string
+	// var lastName string
+	// var email string
+	// var userTickets uint
+
+	// // getting user input
+	// fmt.Println("Enter your firstName: ")
+	// fmt.Scan(&firstName) //store variable in memory address
+	// fmt.Println("Enter your lastName: ")
+	// fmt.Scan(&lastName) //store variable in memory address
+	// fmt.Println("Enter your email: ")
+	// fmt.Scan(&email) //store variable in memory address
+	// fmt.Println("Enter number of tickets: ")
+	// fmt.Scan(&userTickets) //store variable in memory address
+
+	// remainingTickets = remainingTickets - userTickets
+	// // bookings[0] = firstName + " " + lastName  //array
+	// bookings = append(bookings, firstName+" "+lastName) //slice
+	// // fmt.Printf("The whole array: %v\n", bookings)
+	// // fmt.Printf("The first value: %v\n", bookings[0])
+	// // fmt.Printf("Array Type: %T\n", bookings)
+	// // fmt.Printf("Length of Array: %v\n", len(bookings))
+	// fmt.Printf("The whole slice: %v\n", bookings)
+	// fmt.Printf("The first value: %v\n", bookings[0])
+	// fmt.Printf("Slice Type: %T\n", bookings)
+	// fmt.Printf("Length of Slice: %v\n", len(bookings))
+	// fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	// fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	// fmt.Printf("These are all our bookings: %v\n", bookings)
+
 }
 
-func GetTokenbyte(token string) []byte {
-	data := url.Values{}
-	data.Set("grant_type", "client")
-	data.Set("clnt_id", "123")
-	data.Set("resource", "qwer")
-	data.Set("client_assertion", strings.TrimSpace(token))
+// package main
 
-	client := &http.client{}
-	r, err := http.NewRequest("POST", "url", strings.NewReader(data.Encode()))
-	if err != nil {
-		fmt.Println(err)
-	}
-	r.Header.Add("Content-Type", "application/json")
-	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// )
 
-	res, err := client.err
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer res.Body.Close()
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Token BOdy: %v---%T", string(body), body)
-	result := map[string]any{}
-	json.Unmarshal(body, &result)
-	fmt.Printf("%v", result["access_token"])
-}
+// func main() {
+// 	// Define a slice of maps
+// 	bookings := make([]map[string]interface{}, 0)
 
-func getSha1() string {
-	pemContent, err := os.ReadFile("/pki")
-	if err != nil {
-		panic(err)
-	}
-	block, _ := pem.Decode(pemContent)
-	if block == nil {
-		panic("faied tp parse pem file")
-	}
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if cert != nil {
-		panic(err)
-	}
-	fingerprint := sha1.Sum(cert.Raw)
-	var buf bytes.Buffer
-	for i, f := range fingerprint {
-		if i > 0 {
-			fmt.Fprintf(&buf, ":")
-		}
-		fmt.Fprintf(&buf, "%02x", f)
-	}
-	fmt.Printf("Fingerprint %v", buf.String())
-	return strings.ReplaceAll(buf.String(), ":", "")
-}
+// 	// Data to populate the slice
+// 	names := []string{"Alice", "Bob", "Charlie"}
+// 	ages := []int{28, 34, 22}
+
+// 	// Dynamically create maps and append to the slice
+// 	for i := range names {
+// 		booking := map[string]interface{}{
+// 			"name": names[i],
+// 			"age":  ages[i],
+// 		}
+// 		bookings = append(bookings, booking)
+// 	}
+// 	bookingsJSON, err := json.Marshal(bookings)
+// 	if err != nil {
+// 		log.Fatalf("Error marshalling bookings to JSON: %v", err)
+// 	}
+
+// 	// Print the JSON string
+// 	fmt.Println(string(bookingsJSON))
+// 	// Print the slice of maps
+// 	// for i, booking := range bookings {
+// 	// 	fmt.Printf("Booking %d: %v\n", i+1, booking)
+// 	// }
+// }
